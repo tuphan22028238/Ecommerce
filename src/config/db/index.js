1118d09f2d0b54053a29e10dec5fe0bb1009a17a
AnhTuan <125129895+anhtuan23004@users.noreply.github.com>
2024-04-07 16:08:04 +0700

    Test database connection

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,13 +1,22 @@
-const mysql = require('mysql2')
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    port: '3307',
-    user: 'root',
-    password: '123456',
-    database: 'dicdb'
-})
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "123456",
+  database: "E_shop",
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
 
-db.connect()
+db.execute("SELECT * FROM `orders`", function (err, results, fields) {
+  console.log(results);
+});
 
-module.exports = db
+module.exports = db;
