effd6179843086e0a089f27a428a46402f29eb9d
hung <hungnbc2@gmai.com>
2024-05-17 21:27:03 +0700

    feat: Add function for canceling orders and viewing orders for users

--- Backend/src/routes/user.js
+++ Backend/src/routes/user.js
@@ -13,7 +13,9 @@ route.delete("/cart/delete/:userId/:productId", userController.deleteFromCart);
 route.put("/cart/update/:id", userController.updateCart);
 
 // Order
+route.get("/order/:id", userController.viewOrder);
 route.post("/cart/prepare/:id", userController.getPrepareOrderFromCart);
 route.post("/cart/checkout/:id", userController.checkOutOrderFromCart);
+route.put("/order/cancel/:id", userController.cancelOrder);
 
 module.exports = route;
